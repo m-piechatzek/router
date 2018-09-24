ed426a75b8aeb22b8d421148ab87105ec753c404
MonikaP <piechatzek@hotmail.com>
2018-09-23 23:01:15 -0600

    added posts new

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
+import _ from 'lodash'
 class PostsIndex extends React.Component {
+	componentDidMount() {
+		this.props.fetchPosts()
+	}
+
+	renderPosts() {
+		return _.map(this.props.posts, post => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					{post.title}
+				</li>
+			)
+		})
+	}
+
 	render() {
-		return <div>Posts Index</div>
+		return (
+			<div>
+				<h3>Posts</h3>
+				<ul className="list-group">{this.renderPosts()}</ul>
+			</div>
+		)
 	}
 }
 
-export default PostsIndex
+function mapStateToProps(state) {
+	return { posts: state.posts }
+}
+
+export default connect(
+	mapStateToProps,
+	{ fetchPosts }
+)(PostsIndex)
---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class PostsNew extends React.Component {
+	render() {
+		return (
+			<div>
+				<div>PostsNew!</div>
+			</div>
+		)
+	}
+}
+
+export default PostsNew
