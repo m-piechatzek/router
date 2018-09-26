192d1055fbd7bd8f7194fe3722d6410ad6096396
MonikaP <piechatzek@hotmail.com>
2018-09-26 14:28:35 -0600

    Added cancel button and error checks

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
 
 class PostsNew extends React.Component {
 	renderField(field) {
+		const {
+			meta: { touched, error }
+		} = field
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input className="form-control" type="text" {...field.input} />
+				<div>{touched ? error : ''}</div>
 			</div>
 		)
 	}
 
+	onSubmit(values) {
+		console.log(values)
+	}
+
 	render() {
+		const { handleSubmit } = this.props
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<Field
 					label="Title"
 					name="title"
@@ -29,6 +42,12 @@ class PostsNew extends React.Component {
 					name="content"
 					component={this.renderField}
 				/>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+				<Link to="/" className="btn btn-danger">
+					Cancel
+				</Link>
 			</form>
 		)
 	}
