a09a55990850d67a84f198c591d77c2c3cda97ad
MonikaP <piechatzek@hotmail.com>
2018-09-25 18:32:36 -0600

    Added redux-form and updated PostNew with input

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends React.Component {
+	renderTitleField(field) {
+		return (
+			<div className="form-group">
+				<label>Title</label>
+				<input className="form-control" type="text" {...field.input} />
+			</div>
+		)
+	}
+
 	render() {
 		return (
-			<div>
-				<div>PostsNew!</div>
-			</div>
+			<form>
+				<Field name="title" component={this.renderTitleField} />
+			</form>
 		)
 	}
 }
 
-export default PostsNew
+export default reduxForm({
+	form: 'PostsNewForm'
+})(PostsNew)
